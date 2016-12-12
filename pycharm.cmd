1c8bfeec9a89d04d04822c74e64d3121376f4f65
Denis Stepulenok <super.denis@gmail.com>
2016-12-13 02:56:10 +0300

    Far manager

--- pycharm.cmd
+++ pycharm.cmd
@@ -5,5 +5,5 @@ IF NOT EXIST "%PyCharmDist%" (
   echo PyCharm Community Edition download to %DIST%
   explorer "https://www.jetbrains.com/pycharm/download/#section=windows"
 ) else (
-
+  REG QUERY "KeyName" /v ValueName
 )
